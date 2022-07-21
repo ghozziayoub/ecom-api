325b1eded55dcec699ed46444e9ca179343d4ba4
Ayoub Ghozzi <ghozziayoub@gmail.com>
2022-07-21 13:57:48 +0100

    adding my first get api

---
+++ README.md
@@ -0,0 +1,17 @@
+API = Function
+Nom => METHODE + ROUTE
+Traitement
+
+URL : http://localhost:4200/admin/login
+DNS : http://localhost:4200
+ROUTE : /admin/login
+PATH : admin/login
+
+REST : Protocole
+METHODE :         POST | PATCH | GET | DELETE
+DATA : PARAMS   |  OP  |   OP  |  OP |   OP
+       HEADER   |  OP  |   OP  |  OP |   OP
+       BODY     |  OB  |   OB  |  X  |   X
+         
+
+FRONT -> API -> TRAITEMENT -> FRONT
--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const app = express()
 
 const port = 3001
 
-app.listen(port, () => {
-    console.log(`🟢 server started on port ${port}`);
+// p1 : objet : Request => methodes t5alina nrécupériw des données eli jewna mel front
+// p2 : objet : Response => methodes y5aliwna nab3thou des données lel front
+app.get("/firstname/:abc/lastname/:xyz", (req, res) => {
+
+    let myName = req.params.abc
+    let myLastname = req.params.xyz
+
+    res.send({ name: `${myName} ${myLastname}` }) // return name
 })
+
+app.listen(port, () => console.log(`🟢 server started on port ${port}`))
