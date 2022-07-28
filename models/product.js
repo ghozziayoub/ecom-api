aeea8eaed728cf1cb8b6fc773db7c974f3f4babb
ChaouachiYasmine <chaouachiyasmine28@gmail.com>
2022-07-28 11:24:05 +0100

    add model product

---
+++ models/product.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose")
+
+const productSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: false
+        },
+        image: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        price: {
+            type: Number,
+            required: true,
+            unique: false
+        },
+        description: {
+            type: String,
+            required: true,
+            unique: false
+        },
+        idSubCategory: {
+            type: Number,
+            required: true,
+            unique: true
+        },
+        idCategory: {
+            type: Number,
+            required: true,
+            unique: true
+        },
+    }
+)
+
+const Product = mongoose.model("product", productSchema)
+
+module.exports = Product
